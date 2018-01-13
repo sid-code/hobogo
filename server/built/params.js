8ba5b96779a7b01d85f59d6d8d02f43524d42ef9
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-01-12 23:38:26 -0700

    Remove built directory and add it to .gitignore

--- server/built/params.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function makeQueryString(params) {
-    var esc = encodeURIComponent;
-    return Object.keys(params)
-        .map(k => esc(k) + '=' + esc(params[k]))
-        .join('&');
-}
-exports.default = makeQueryString;
