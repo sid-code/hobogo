02972fda6ff472b1c4de6890c017d104759cd6b6
Sidharth Kulkarni <kulkarnisidharth1@gmail.com>
2018-01-13 14:17:58 -0700

    fix stuff but not really

--- client/selector.js
+++ client/selector.js
@@ -3,69 +3,75 @@ var destinations = [];
 
 var url = 'https://cors-anywhere.herokuapp.com/https://api.skypicker.com/locations';
 
-$('#select-city, #select-origin').selectize({
-  valueField: 'name',
-  labelField: 'name',
-  searchField: ['name','code'],
-  options: [],
-  delimiter: ',',
-  render: {
-    option: function(item, escape) {
-      return getOption(item, escape);
+function doit(sel, maxItems) {
+  $(sel).selectize({
+    valueField: 'name',
+    maxItems: maxItems,
+    labelField: 'name',
+    searchField: ['name','code'],
+    options: [],
+    delimiter: ',',
+    render: {
+      option: function(item, escape) {
+        return getOption(item, escape);
+      },
+      item: function(item, escape){
+        return getOption(item, escape);
+      }
     },
-    item: function(item, escape){
-      return getOption(item, escape);
-    }
-  },
-  onItemAdd: function(value, $item) {
-    console.log(value);
-    if(getMarkerIndex(value) == -1){
-      addAirport(value);
-    }
-  },
-  onItemRemove: function(value) {
-    console.log(value);
-    removeAirport(getMarkerIndex(value));
-  },
-  load: function(query, callback) {
-    if (!query.length) return callback();
-    $.ajax({
-      url: url,
-      type: 'GET',
-      dataType: 'json',
-      data: {
-	term: query,
-	locale: 'en-US',
-	location_types: 'city',
-	limit: '100',
-      },
-      error: function() {
-	callback();
-      },
-      success: function(res) {
-	console.log(res.locations);
-	callback(res.locations);
+    onItemAdd: function(value, $item) {
+      console.log(value);
+      if(getMarkerIndex(value) == -1){
+        addAirport(value);
       }
-    });
-    $.ajax({
-      url: url,
-      type: 'GET',
-      dataType: 'json',
-      data: {
-	term: query,
-	locale: 'en-US',
-	location_types: 'airport',
-	limit: '100',
-      },
-      error: function() {
-	callback();
-      },
-      success: function(res) {
-	callback(res.locations);
-      }
-    });
-  }
-});
+    },
+    onItemRemove: function(value) {
+      console.log(value);
+      removeAirport(getMarkerIndex(value));
+    },
+    load: function(query, callback) {
+      if (!query.length) return callback();
+      $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        data: {
+          term: query,
+          locale: 'en-US',
+          location_types: 'city',
+          limit: '100',
+        },
+        error: function() {
+          callback();
+        },
+        success: function(res) {
+          console.log(res.locations);
+          callback(res.locations);
+        }
+      });
+      $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: 'json',
+        data: {
+          term: query,
+          locale: 'en-US',
+          location_types: 'airport',
+          limit: '100',
+        },
+        error: function() {
+          callback();
+        },
+        success: function(res) {
+          callback(res.locations);
+        }
+      });
+    }
+  });
+}
+
+doit("#select-city");
+doit("#select-origin", 1);
 
 function getOption(item, escape){
   return '<div>' +
